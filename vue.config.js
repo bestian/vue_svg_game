b37ce33debe7b9bb402bfd5c618b29d8d8b4c8a9
Bestian Tang <bestian@gmail.com>
2020-10-25 16:20:02 +0800

    Update vue.config.js
    test pwa

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   runtimeCompiler: true,
+  pwa: {
+    workboxOptions: {
+      exclude: ['*.gitkeep'],
+    }
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue_svg_game/'
     : '/'
