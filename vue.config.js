9281d37db92520e9ad78a853ea98f2abac330e13
Bestian Tang <bestian@gmail.com>
2020-10-25 15:26:03 +0800

    pwa test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 module.exports = {
+  pwa: {
+    name: '射飛碟',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/registerServiceWorker.js',
+      // ...other Workbox options...
+    }
+  },
   runtimeCompiler: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue_svg_game/'
