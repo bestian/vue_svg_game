ddda8f4c68b5a5550919249428cf927efd784822
Bestian Tang <bestian@gmail.com>
2020-10-31 09:20:48 +0800

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="about">
-    <h1>遊戲說明</h1>
-    <p>
-      大家好！這是一個射飛碟的小遊戲
-    </p>
-    <p>
-      射中一個飛碟得5金
-    </p>
-    <p>
-      100金換一顆心
-    </p>
-    <p>
-      飛碟落地就扣一顆心
-    </p>
-    <p>
-      扣完心就結束了
-    </p>
-    <p>
-      遊戲有四種難度，你可以任意選擇喔
-    </p>
+    <div>
+      <h1>遊戲說明</h1>
+      <p>
+        大家好！這是一個射飛碟的小遊戲
+      </p>
+      <p>
+        射中一個飛碟得5金
+      </p>
+      <p>
+        100金換一顆心
+      </p>
+      <p>
+        飛碟落地就扣一顆心
+      </p>
+      <p>
+        扣完心就結束了
+      </p>
+      <p>
+        遊戲有四種難度，你可以任意選擇喔
+      </p>
+    </div>
   </div>
 </template>
 
@@ -27,7 +29,7 @@
 
 <style>
 
-.about {
+.about{
   position: fixed;
   top: 0;
   left: 0;
@@ -36,6 +38,12 @@
   height: 100vh;
   color: white;
   font-size: 18px;
+}
+
+.about div {
+  max-width: 420px;
+  margin: 0 auto;
+  text-align: left;
 }
 
 </style>
