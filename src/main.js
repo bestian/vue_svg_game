d68dcaba50050988a539ff0a740f78c907e64c67
Bestian Tang <bestian@gmail.com>
2021-01-02 21:54:08 +0800

    lighthouse improved

--- src/main.js
+++ src/main.js
@@ -1,22 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import store from './store/store'
 
 import router from './router'
-
-import wb from './registerServiceWorker'
-
-Vue.prototype.$workbox = wb
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  render: h => h(App),
   router,
-  components: { App },
-  template: '<App/>',
   store
-})
+}).$mount('#app')
