839c111db1ad0b1ad993114ffc4eb9550b4e77d3
Bestian Tang <bestian@gmail.com>
2021-01-03 13:10:26 +0800

    deploy

---
+++ precache-manifest.006684cf4b22afd195327a858aec5517.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217adb10c8b36aef3dd8",
+    "url": "/vue_svg_game/css/app.270380f5.css"
+  },
+  {
+    "revision": "217adb10c8b36aef3dd8",
+    "url": "/vue_svg_game/css/app.270380f5.css.map"
+  },
+  {
+    "revision": "12ec4f8bc79a727b8f00",
+    "url": "/vue_svg_game/css/chunk-c7176cec.f1ae1ae5.css"
+  },
+  {
+    "revision": "12ec4f8bc79a727b8f00",
+    "url": "/vue_svg_game/css/chunk-c7176cec.f1ae1ae5.css.map"
+  },
+  {
+    "revision": "297e559f7ea403fbeea4251f6c9e64d3",
+    "url": "/vue_svg_game/favicon.ico"
+  },
+  {
+    "revision": "bec44f894b327d8d1375b091cb0ce460",
+    "url": "/vue_svg_game/happy.mp3"
+  },
+  {
+    "revision": "1a345f3832aed955a359ec0461094151",
+    "url": "/vue_svg_game/img/big_ufo.1a345f38.jpg"
+  },
+  {
+    "revision": "4840d1542dcc1b23a9df95c5d2f1499f",
+    "url": "/vue_svg_game/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "c5a45848d7c6bcf9675aed7f3a7030af",
+    "url": "/vue_svg_game/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "4840d1542dcc1b23a9df95c5d2f1499f",
+    "url": "/vue_svg_game/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "c5a45848d7c6bcf9675aed7f3a7030af",
+    "url": "/vue_svg_game/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "4f7580b64a1d7384abd1665f9bab4f1e",
+    "url": "/vue_svg_game/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "3cc37d7daa28f1f0fdceb82e6d04fb77",
+    "url": "/vue_svg_game/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "a4a0268bdcb7f84b106491ada24763b7",
+    "url": "/vue_svg_game/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "f0521e3a7599ec23b7276f59a311fa4f",
+    "url": "/vue_svg_game/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "65a2f50082a3f04c67033fdaa5eb996e",
+    "url": "/vue_svg_game/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "a4a0268bdcb7f84b106491ada24763b7",
+    "url": "/vue_svg_game/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "f8013b830dc1f3f94c712ee064c0df5c",
+    "url": "/vue_svg_game/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "afd315254148630435edee9a80837b78",
+    "url": "/vue_svg_game/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "17bf6b041e9d215fa703d78c3a9abb8e",
+    "url": "/vue_svg_game/img/icons/favicon.ico"
+  },
+  {
+    "revision": "ca9b72d123a80d2915c1292b975dfec1",
+    "url": "/vue_svg_game/img/icons/manifest.json"
+  },
+  {
+    "revision": "a90dd01304a4bf8eb219cc06695f6bc3",
+    "url": "/vue_svg_game/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "1bc121c95b7021ea27f9035c6dd58a94",
+    "url": "/vue_svg_game/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "539c281b6a1044a2bea4ef133df8775a",
+    "url": "/vue_svg_game/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "3d1d6d92fd84d10a9af6331d25cc3572",
+    "url": "/vue_svg_game/index.html"
+  },
+  {
+    "revision": "217adb10c8b36aef3dd8",
+    "url": "/vue_svg_game/js/app.624a3cff.js"
+  },
+  {
+    "revision": "217adb10c8b36aef3dd8",
+    "url": "/vue_svg_game/js/app.624a3cff.js.map"
+  },
+  {
+    "revision": "12ec4f8bc79a727b8f00",
+    "url": "/vue_svg_game/js/chunk-c7176cec.0227c979.js"
+  },
+  {
+    "revision": "12ec4f8bc79a727b8f00",
+    "url": "/vue_svg_game/js/chunk-c7176cec.0227c979.js.map"
+  },
+  {
+    "revision": "4acd246d3e2886e6ed8e",
+    "url": "/vue_svg_game/js/chunk-vendors.1b139f79.js"
+  },
+  {
+    "revision": "4acd246d3e2886e6ed8e",
+    "url": "/vue_svg_game/js/chunk-vendors.1b139f79.js.map"
+  },
+  {
+    "revision": "e7f1b5b2b27def404a717a594b0ecdfc",
+    "url": "/vue_svg_game/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue_svg_game/robots.txt"
+  },
+  {
+    "revision": "e27ef33d7912f75f7deb4a34dbe5e92b",
+    "url": "/vue_svg_game/static/android-chrome-192x192.png"
+  },
+  {
+    "revision": "813ced840fbe99880722be6908f07b11",
+    "url": "/vue_svg_game/static/android-chrome-512x512.png"
+  },
+  {
+    "revision": "698a420888abb68a0f2d15905259a36a",
+    "url": "/vue_svg_game/static/apple-touch-icon.png"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "/vue_svg_game/static/browserconfig.xml"
+  },
+  {
+    "revision": "24503a01bd59180abe7b8845afc5daac",
+    "url": "/vue_svg_game/static/favicon-16x16.png"
+  },
+  {
+    "revision": "d74f87cc1cb37f7766b14dc5cfa562f3",
+    "url": "/vue_svg_game/static/favicon-32x32.png"
+  },
+  {
+    "revision": "297e559f7ea403fbeea4251f6c9e64d3",
+    "url": "/vue_svg_game/static/favicon.ico"
+  },
+  {
+    "revision": "4840d1542dcc1b23a9df95c5d2f1499f",
+    "url": "/vue_svg_game/static/img/android-chrome-192x192.png"
+  },
+  {
+    "revision": "c5a45848d7c6bcf9675aed7f3a7030af",
+    "url": "/vue_svg_game/static/img/android-chrome-512x512.png"
+  },
+  {
+    "revision": "4840d1542dcc1b23a9df95c5d2f1499f",
+    "url": "/vue_svg_game/static/img/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "c5a45848d7c6bcf9675aed7f3a7030af",
+    "url": "/vue_svg_game/static/img/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "4f7580b64a1d7384abd1665f9bab4f1e",
+    "url": "/vue_svg_game/static/img/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "3cc37d7daa28f1f0fdceb82e6d04fb77",
+    "url": "/vue_svg_game/static/img/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "a4a0268bdcb7f84b106491ada24763b7",
+    "url": "/vue_svg_game/static/img/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "f0521e3a7599ec23b7276f59a311fa4f",
+    "url": "/vue_svg_game/static/img/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "65a2f50082a3f04c67033fdaa5eb996e",
+    "url": "/vue_svg_game/static/img/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "a4a0268bdcb7f84b106491ada24763b7",
+    "url": "/vue_svg_game/static/img/apple-touch-icon.png"
+  },
+  {
+    "revision": "f8013b830dc1f3f94c712ee064c0df5c",
+    "url": "/vue_svg_game/static/img/favicon-16x16.png"
+  },
+  {
+    "revision": "afd315254148630435edee9a80837b78",
+    "url": "/vue_svg_game/static/img/favicon-32x32.png"
+  },
+  {
+    "revision": "17bf6b041e9d215fa703d78c3a9abb8e",
+    "url": "/vue_svg_game/static/img/favicon.ico"
+  },
+  {
+    "revision": "ca9b72d123a80d2915c1292b975dfec1",
+    "url": "/vue_svg_game/static/img/manifest.json"
+  },
+  {
+    "revision": "a90dd01304a4bf8eb219cc06695f6bc3",
+    "url": "/vue_svg_game/static/img/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "1bc121c95b7021ea27f9035c6dd58a94",
+    "url": "/vue_svg_game/static/img/mstile-150x150.png"
+  },
+  {
+    "revision": "539c281b6a1044a2bea4ef133df8775a",
+    "url": "/vue_svg_game/static/img/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "82bb5d2db069ae6fa2e58b5fdc8b0e3e",
+    "url": "/vue_svg_game/static/mstile-150x150.png"
+  },
+  {
+    "revision": "ce7b58aae9eae5f751c67e0f2a7bea31",
+    "url": "/vue_svg_game/static/og-image.jpg"
+  },
+  {
+    "revision": "0337e82995da7fdd5c3d580916a14b08",
+    "url": "/vue_svg_game/static/og-image.png"
+  },
+  {
+    "revision": "108b5978d7e1d93f638a60e4846e89da",
+    "url": "/vue_svg_game/static/site.webmanifest"
+  }
+]);
